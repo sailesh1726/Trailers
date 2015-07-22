149ab5115c93bfcf24f20dbcb718cf90c83f2fa6
sailesh1726 <sailesh1726@gmail.com>
2015-07-22 16:48:51 -0400

    Youtube API added
    enjoy

--- app/src/main/java/com/example/android/trailers/MainActivity.java
+++ app/src/main/java/com/example/android/trailers/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.android.trailers;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends YouTubeBaseActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
