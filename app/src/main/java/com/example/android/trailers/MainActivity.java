f292db7623c9e6d2e17f70f0253c7cdd371488e7
sailesh1726 <sailesh1726@gmail.com>
2015-09-20 20:14:22 -0700

    Toolbar

--- app/src/main/java/com/example/android/trailers/MainActivity.java
+++ app/src/main/java/com/example/android/trailers/MainActivity.java
@@ -1,65 +1,28 @@
 package com.example.android.trailers;
 
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-import com.google.android.youtube.player.YouTubeBaseActivity;
+public class MainActivity extends AppCompatActivity {
 
-import java.util.ArrayList;
-import java.util.Arrays;
+protected static String TAG= MainActivity.class.getName();
 
-
-public class MainActivity extends YouTubeBaseActivity {
-
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
-    private ArrayList<String> trailerArrayList;
-
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /** attaching layout xml **/
         setContentView(R.layout.activity_main);
 
-        String[] tempArray=getResources().getStringArray(R.array.sampleArray);
+        toolbar= (Toolbar) findViewById(R.id.app_bar);
 
-        trailerArrayList= new ArrayList<String>(Arrays.asList(tempArray));
+        setSupportActionBar(toolbar);
 
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        recyclerView.setHasFixedSize(true);
-
-        layoutManager= new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        adapter = new TrailerAdapter(trailerArrayList);
-
-        recyclerView.setAdapter(adapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
