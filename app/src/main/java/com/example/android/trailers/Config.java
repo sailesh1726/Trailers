76be77e086d9ebe8cd95ff2e3230ab5dfc4d5b76
sailesh1726 <sailesh1726@gmail.com>
2015-10-14 01:41:54 -0700

    rotten key
    lets start

--- app/src/main/java/com/example/android/trailers/Config.java
+++ app/src/main/java/com/example/android/trailers/Config.java
@@ -7,4 +7,8 @@ public class Config {
 
     // YouTube video id
     public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+
+    // Rotten tomatoes API KEY
+    public static final String ROTTEN_TOMATOES_KEY = "5sxu5zqt7v3xhwddt9ppdxjg";
+
 }
